5641d2d92ef6285b2161b631ba394dcb41452251
MrGuBerry <46570156+MrGuBerry@users.noreply.github.com>
2019-03-02 19:30:39 +0000

    Add files via upload

---
+++ APP/app.js
@@ -0,0 +1,69 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require("express-session");
+
+var index = require('./routes/index');
+var home = require('./routes/home');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.jpg')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'nw87vwn53w8n5yf83sk5snk7s85vsnvg37tgs',
+  resave: true,
+  saveUninitialized: false
+}));
+
+app.use('/', index);
+app.use('/home', home);
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+
+
+
+
+
+
+
+
+
+
+module.exports = app;
+
+app.listen(3000, function(err){
+    console.log('server is running');
+})
